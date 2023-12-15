214f60e2d8d92ab75c0263633c6deee79936d85f
Kamil <majcherczak.kamil@protonmail.com>
2023-12-15 16:16:39 +0100

    competition-tab component

--- src/app/competition-tab/competition-tab.component.ts
+++ src/app/competition-tab/competition-tab.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ParticipantsComponent } from '../participants/participants.component';
 
 @Component({
   selector: 'app-competition-tab',
   standalone: true,
-  imports: [],
+  imports: [ParticipantsComponent],
   templateUrl: './competition-tab.component.html',
   styleUrl: './competition-tab.component.css'
 })
--- src/app/competitions/competitions.component.ts
+++ src/app/competitions/competitions.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CompetitionTabComponent } from '../competition-tab/competition-tab.component';
 
 @Component({
   selector: 'app-competitions',
   standalone: true,
-  imports: [],
+  imports: [CompetitionTabComponent],
   templateUrl: './competitions.component.html',
   styleUrl: './competitions.component.css'
 })
