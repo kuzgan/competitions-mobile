17ad41e8b2afa78d84ccecdc174d7ec656dfa6cc
Kamil <majcherczak.kamil@protonmail.com>
2023-12-15 16:42:50 +0100

    add button

--- src/app/competition-tab/competition-tab.component.ts
+++ src/app/competition-tab/competition-tab.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component,  } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ParticipantsComponent } from '../participants/participants.component';
 
 @Component({
   selector: 'app-competition-tab',
   standalone: true,
-  imports: [ParticipantsComponent],
+  imports: [
+    ParticipantsComponent,
+    CommonModule,
+  ],
   templateUrl: './competition-tab.component.html',
   styleUrl: './competition-tab.component.css'
 })
 export class CompetitionTabComponent {
+  tooLateToJoin: (isTooLate: boolean) => boolean = (isTooLate) => {
+    return isTooLate;
+  }
+
 
 }
