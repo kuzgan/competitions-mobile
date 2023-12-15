9d129ab6f98bddfbc8d7bd731d015c854fc1e7cb
Kamil <majcherczak.kamil@protonmail.com>
2023-12-15 22:42:43 +0100

    update

--- src/app/competition-tab/competition-tab.component.ts
+++ src/app/competition-tab/competition-tab.component.ts
@@ -1,18 +1,23 @@
 import { Component,  } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ParticipantsComponent } from '../participants/participants.component';
+import { LeaderBoardComponent } from '../leader-board/leader-board.component';
 
 @Component({
   selector: 'app-competition-tab',
   standalone: true,
   imports: [
-    ParticipantsComponent,
     CommonModule,
+    ParticipantsComponent,
+    LeaderBoardComponent,
   ],
   templateUrl: './competition-tab.component.html',
   styleUrl: './competition-tab.component.css'
 })
 export class CompetitionTabComponent {
+  userTag: string = "@rog13k"
+  gameTag: string = "@xBits prize"
+
   tooLateToJoin: (isTooLate: boolean) => boolean = (isTooLate) => {
     return isTooLate;
   }
